baa6f0d63c103417e17d141b280dd5a29bdcb890
WessCoby <cobygiven@gmail.com>
2019-06-21 22:58:00 +0000

    Fixed bugs and syntax. Everything working corectly now.

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 const fs = require('fs'),
     path = require('path'),
-    exec = require('child_process').exec,
-    progress = require('progress'),
+    util = require('util'),
+    { exec } = require('child_process'),
+    promisified_exec = util.promisify(exec),
+    ProgressBar = require('progress'),
     { log } = console;
 
+async function line_count() {
+        let jsonData = [];
 
-const lines = exec('cat ./data.csv | wc -l');
-const read_heading = exec('read -r line < ./data.csv && echo $line');
-const read_body = exec("./readlines ./data.csv");
+        const collect = data => {
+			let record = {};
+			data = data.replace("\n", "").split(',');
+			for(let element = 0; element < data.length; element++) {
+				record[headings[element]] = data[element];
+			}
+			jsonData.push(record);
+        };
 
-let headings = [], jsonData = [];
+        let lines = await promisified_exec(`cat ${process.argv[2]} | wc -l`);
+        lines = lines.stdout;
+        const bar = new ProgressBar('Converting File [:bar>] Lines processed: :current/:total | :percent complete | :etas remaining', {
+                complete: '=',
+                incomplete: ' ',
+                width: 30,
+                total: lines - 1        // Not counting header line
+        });
 
-const collect = data => {
-        let record = {};
-        data = data.replace("\n", "").split(',');
-        for(let element = 0; element < data.length; element++) {
-              record[headings[element]] = data[element];
-        }
-        // log(record);
-        jsonData.push(record);
-};
+        let headings = await promisified_exec(`read -r line < ${process.argv[2]} && echo $line`);
+        headings = headings.stdout.replace('\n', '').split(',');
+        
+        const read_body = exec("./readlines ./data.csv");
 
-read_heading.stdout.on('data', head => {
-        headings = head.replace("\n", "").split(",");
-});
+        
+        log('headings: ', headings)
+        read_body.stdout.on('data', data => {
+                collect(data);
+                bar.tick();
+        });
+        read_body.on('close', () => {
+                log('Done!!!');
+                log(jsonData);
+        });
+}
 
-read_heading.on('close', () => {
-
-        read_body.stdout.on('data', collect);
-});
-
-read_body.on('close', () => log(JSON.stringify(jsonData)));
+line_count();
