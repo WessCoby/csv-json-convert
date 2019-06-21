2ab67c54a882588af4635c8a25c7072a7f162187
WessCoby <cobygiven@gmail.com>
2019-06-21 23:21:27 +0000

    Refactoring: added comments and simplified syntax where necessary

--- index.js
+++ index.js
@@ -6,42 +6,48 @@ const fs = require('fs'),
     ProgressBar = require('progress'),
     { log } = console;
 
-async function line_count() {
-        let jsonData = [];
+async function collect_csv_data() {
+	let jsonData = { data: [] };
 
-        const collect = data => {
-			let record = {};
-			data = data.replace("\n", "").split(',');
-			for(let element = 0; element < data.length; element++) {
-				record[headings[element]] = data[element];
-			}
-			jsonData.push(record);
-        };
+	// Push data from the csv file to jsonData.data 
+	const collect = data => {
+		let record = {};
+		data = data.replace("\n", "").split(',');
+		for(let element = 0; element < data.length; element++) {
+			record[headings[element]] = data[element];
+		}
+		jsonData['data'].push(record);
+	};
 
-        let lines = await promisified_exec(`cat ${process.argv[2]} | wc -l`);
-        lines = lines.stdout;
-        const bar = new ProgressBar('Converting File [:bar>] Lines processed: :current/:total | :percent complete | :etas remaining', {
-                complete: '=',
-                incomplete: ' ',
-                width: 30,
-                total: lines - 1        // Not counting header line
-        });
+	// Get number of lines in file (header excluded)
+	let lines = await promisified_exec(`cat ${process.argv[2]} | wc -l`);
+	lines = lines.stdout - 1;
 
-        let headings = await promisified_exec(`read -r line < ${process.argv[2]} && echo $line`);
-        headings = headings.stdout.replace('\n', '').split(',');
-        
-        const read_body = exec("./readlines ./data.csv");
+	// Progress Bar
+	const bar = new ProgressBar('Converting File [:bar] Lines processed: :current/:total | :percent complete | :etas remaining', {
+			complete: '=',
+			incomplete: ' ',
+			width: 30,
+			total: lines
+	});
 
-        
-        log('headings: ', headings)
-        read_body.stdout.on('data', data => {
-                collect(data);
-                bar.tick();
-        });
-        read_body.on('close', () => {
-                log('Done!!!');
-                log(jsonData);
-        });
+
+	// Get first line from file [headings line]
+	let headings = await promisified_exec(`read -r line < ${process.argv[2]} && echo $line`);
+	headings = headings.stdout.replace('\n', '').split(',');
+	
+
+	// Get contents from file, line by line
+	const content = exec(`./readlines ${process.argv[2]}`);
+	content.stdout.on('data', data => {
+			collect(data);
+			bar.tick(); // Update progress
+	});
+	// Once Complete
+	content.on('close', () => {
+			log(jsonData);
+			// call function to save data to new file and output completion message
+	});
 }
 
-line_count();
+collect_csv_data();
